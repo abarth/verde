8ebf0fe0e617cc549e01d4c9660291272f107287
Adam Barth <abarth@chromium.org>
2015-12-19 19:48:22 -0800

    Add more terrible build infrastructure

--- tools/update_ssd.sh
+++ tools/update_ssd.sh
@@ -5,7 +5,13 @@ VERDE_ROOT=$(dirname $(dirname $(dirname $(readlink -f ${BASH_SOURCE[0]}))))
 set -ex
 
 (cd $VERDE_ROOT/linux && make -j2 drivers/staging/verde/verde_ipc.ko)
+(cd $VERDE_ROOT/verde/userland/hello && make)
+(cd $VERDE_ROOT/verde/userland/ipc_testbed && make)
 
 sudo mount -n /dev/deepzen-vg/lv_verde $VERDE_ROOT/out/ssd
+
 sudo cp $VERDE_ROOT/linux/drivers/staging/verde/verde_ipc.ko $VERDE_ROOT/out/ssd/home
+sudo cp $VERDE_ROOT/verde/userland/hello/hello $VERDE_ROOT/out/ssd/home
+sudo cp $VERDE_ROOT/verde/userland/ipc_testbed/ipc_testbed $VERDE_ROOT/out/ssd/home
+
 sudo umount $VERDE_ROOT/out/ssd
